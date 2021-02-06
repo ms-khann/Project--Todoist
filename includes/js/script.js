d0b9acbed6b672966894fb70d4efbe6928518e4c
ms-khann <ms.solo786@gmail.com>
2021-02-06 15:40:26 +0530

    updated

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded',function(){
-    let open_sidebar = document.getElementById("open-sidebar");
-    let sidebar = document.getElementById("sidebar");
-    let bg_layer = document.getElementById("bg-layer");
-    let open_search_box = document.getElementById("open-search-box");
-    let search_box = document.getElementById("search-box");
-    let close_search_box = document.getElementById("close-search-box");
-    let accordian_toggle = document.querySelectorAll(".accordian-container .accordian-toggle");
+    const sidebar = document.getElementById("sidebar");
+    const open_sidebar = document.getElementById("open-sidebar");
+    const bg_layer = document.getElementById("bg-layer");
+    const open_search_box = document.getElementById("open-search-box");
+    const search_box = document.getElementById("search-box");
+    const close_search_box = document.getElementById("close-search-box");
+    const accordian_toggle = document.querySelectorAll(".accordian-container .accordian-toggle");
+    const add_project = document.getElementsByClassName('add-project');
+    const add_project_popup = document.getElementById('add-project-popup');
+    const close_project_popup = document.getElementById('close-project-popup');
+    const open_color_list = document.getElementById('open-color-list');
+    const color_list_popup = document.getElementById('color-list-popup');
+    const open_parent_project_popup = document.getElementById('open-parent-project-popup');
+    const parent_project_popup = document.getElementById('parent-project-popup');
     for(let current of accordian_toggle){
         console.log(current);
         current.addEventListener('click',function(event){
@@ -13,21 +20,70 @@ document.addEventListener('DOMContentLoaded',function(){
         });
     }
     open_sidebar.addEventListener('click',function(){
-        sidebar.classList += " active";
-        bg_layer.classList += " active";
+        openSideBar();
     });
     bg_layer.addEventListener("click",function(){
-        sidebar.classList.remove('active');
-        this.classList.remove('active');
+        // bgLayer();
+        closeSideBar();
+        closeColorList();
+        closeParentProjectPopup();
     });
     open_search_box.addEventListener('click',function(event){
         console.log(event);
         let t_origin = event.x + ' ' + event.y;
         // console.log(t_origin);
         // search_box.style.transformOrigin = '\"'+t_origin+'\"';
-        search_box.classList += " active";
+        search_box.classList.add('active');
     });
     close_search_box.addEventListener("click",function(){
         search_box.classList.remove("active");
     });
+
+    // --- Start:Open Add Project Popup ----
+    for(let project of add_project){
+        project.addEventListener('click',function(){
+            add_project_popup.classList.add('active');
+            closeSideBar();
+        });
+    }
+    // --- End:Open Add Project Popup ----
+
+    // --- Start:Close Add Project Popup ----
+    close_project_popup.addEventListener('click',function(){
+        add_project_popup.classList.remove('active');
+    });
+    // --- End:Close Add Project Popup ----
+
+    // --- Start : Open Color List ---
+    open_color_list.addEventListener('click',function(){
+        color_list_popup.classList.add('active');
+        showBgLayer();
+    });
+    // --- End : Open Color List ---
+
+
+    // --- Start: Parent Project Popup ---
+    open_parent_project_popup.addEventListener("click",function(){
+        parent_project_popup.classList.add('active');
+        showBgLayer();
+    });
+    // --- End: Parent Project Popup ---
+
+    function openSideBar(){
+        sidebar.classList.add('active');
+        showBgLayer();
+    }
+    function closeSideBar(){
+        sidebar.classList.remove('active');
+        bg_layer.classList.remove('active');
+    }
+    function showBgLayer(){
+        bg_layer.classList.add('active');
+    }
+    function closeColorList(){
+        color_list_popup.classList.remove('active');
+    }
+    function closeParentProjectPopup(){
+        parent_project_popup.classList.remove('active');
+    }
 });
